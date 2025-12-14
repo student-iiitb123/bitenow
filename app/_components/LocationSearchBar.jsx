fedfca12c25f0fc3674bbcb7053a3b1734f1a343
Ashish singh Goutam <ashishgautam7696@gmail.com>
2025-12-15 02:25:52 +0530

    add Home page

--- app/_components/LocationSearchBar.jsx
+++ app/_components/LocationSearchBar.jsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 
 export default function LocationInput({loadResturant}) {
   const [locations, setLocations] = useState([]); // all location
-  const [filtered, setFiltered] = useState([]);   // matching locations
+  const [filtered, setFiltered] = useState([]);   // matching location
   const [showDropdown, setShowDropdown] = useState(false); // show/hide dropdown
   const [value, setValue] = useState(""); // input value
 
