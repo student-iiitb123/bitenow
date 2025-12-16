33c33c43efe9a43b832bcbb04126418ee142e0ba
Ashish singh Goutam <ashishgautam7696@gmail.com>
2025-12-16 15:13:15 +0530

    add origin

--- app/api/restuarant/customers/location/route.js
+++ app/api/restuarant/customers/location/route.js
@@ -6,7 +6,6 @@ export async function GET(){
     await dbConnect();
     let success = false;
     let result = await User.find();
-    // console
     result = result.map((items) => items.city.charAt(0).toUpperCase()+ items.city.slice(1))
     result = [...new Set(result.map((item) =>item ))]
     if(result){
