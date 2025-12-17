08bcbe3ef29ba38aa41e12d5e72e00092cefa5e9
Ashish singh Goutam <ashishgautam7696@gmail.com>
2025-12-17 14:18:24 +0530

    add global.css

--- app/explore/[name]/page.jsx
+++ app/explore/[name]/page.jsx
@@ -11,7 +11,7 @@ export default function RestaurantPage() {
   const [restaurant, setRestaurant] = useState(null);
   const [foods, setFoods] = useState([]);
   const [loading, setLoading] = useState(true);
-  //make Sta 
+  //make 
   const[cart,setCart] = useState();
 
 
