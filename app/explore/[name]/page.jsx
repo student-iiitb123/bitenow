2891df6b2eab12743d5285d6cacfaca98c7974aa
Ashish singh Goutam <ashishgautam7696@gmail.com>
2025-12-17 13:05:27 +0530

    add origin

--- app/explore/[name]/page.jsx
+++ app/explore/[name]/page.jsx
@@ -11,7 +11,7 @@ export default function RestaurantPage() {
   const [restaurant, setRestaurant] = useState(null);
   const [foods, setFoods] = useState([]);
   const [loading, setLoading] = useState(true);
-  //make a state for Cart 
+  //make State for Cart 
   const[cart,setCart] = useState();
 
 
