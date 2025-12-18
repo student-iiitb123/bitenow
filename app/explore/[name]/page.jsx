349c9cc7b58f610a36d543e787c039ae33ff0333
Ashish singh Goutam <ashishgautam7696@gmail.com>
2025-12-18 14:49:39 +0530

    add cart Model

--- app/explore/[name]/page.jsx
+++ app/explore/[name]/page.jsx
@@ -59,7 +59,7 @@ useEffect(() => {
   const storedCart = JSON.parse(localStorage.getItem("cart")) || [];
   setCartStorage(storedCart);
 }, []);
-  // const [cartIds,setcartIds] = useState(() => cartStorage.map((item) => {
+  // const [cartIds,setcartIds] = useState(() => cartStorage.map((item
   
   
 
